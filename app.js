d903e237df94a988948d69b23d1bc0c4833a7a70
chohyeonjin <yrusos@naver.com>
2015-12-13 16:53:06 +0900

    added functions of questions

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/surveys', surveys);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
